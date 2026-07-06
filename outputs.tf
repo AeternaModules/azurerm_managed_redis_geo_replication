5db8c309854fe603c98e5ad7bb42ebd46bfde063
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "managed_redis_geo_replications" {
   description = "All managed_redis_geo_replication resources"
   value       = azurerm_managed_redis_geo_replication.managed_redis_geo_replications
 }
+output "managed_redis_geo_replications_linked_managed_redis_ids" {
+  description = "List of linked_managed_redis_ids values across all managed_redis_geo_replications"
+  value       = [for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : v.linked_managed_redis_ids]
+}
+output "managed_redis_geo_replications_managed_redis_id" {
+  description = "List of managed_redis_id values across all managed_redis_geo_replications"
+  value       = [for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : v.managed_redis_id]
+}
 
