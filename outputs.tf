db6186e97554ea9fe482afbbdaf0bc6c33bdfb0a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "managed_redis_geo_replications" {
-  description = "All managed_redis_geo_replication resources"
-  value       = azurerm_managed_redis_geo_replication.managed_redis_geo_replications
-}
 output "managed_redis_geo_replications_linked_managed_redis_ids" {
-  description = "List of linked_managed_redis_ids values across all managed_redis_geo_replications"
-  value       = [for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : v.linked_managed_redis_ids]
+  description = "Map of linked_managed_redis_ids values across all managed_redis_geo_replications, keyed the same as var.managed_redis_geo_replications"
+  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.linked_managed_redis_ids }
 }
 output "managed_redis_geo_replications_managed_redis_id" {
-  description = "List of managed_redis_id values across all managed_redis_geo_replications"
-  value       = [for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : v.managed_redis_id]
+  description = "Map of managed_redis_id values across all managed_redis_geo_replications, keyed the same as var.managed_redis_geo_replications"
+  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.managed_redis_id }
 }
 
