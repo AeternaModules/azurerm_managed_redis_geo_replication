61966dbbdc5ce912c7202fdf5d6c4686d9c6fdfb
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "managed_redis_geo_replications_id" {
   description = "Map of id values across all managed_redis_geo_replications, keyed the same as var.managed_redis_geo_replications"
-  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.id }
+  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "managed_redis_geo_replications_linked_managed_redis_ids" {
   description = "Map of linked_managed_redis_ids values across all managed_redis_geo_replications, keyed the same as var.managed_redis_geo_replications"
-  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.linked_managed_redis_ids }
+  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.linked_managed_redis_ids if v.linked_managed_redis_ids != null && length(v.linked_managed_redis_ids) > 0 }
 }
 output "managed_redis_geo_replications_managed_redis_id" {
   description = "Map of managed_redis_id values across all managed_redis_geo_replications, keyed the same as var.managed_redis_geo_replications"
-  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.managed_redis_id }
+  value       = { for k, v in azurerm_managed_redis_geo_replication.managed_redis_geo_replications : k => v.managed_redis_id if v.managed_redis_id != null && length(v.managed_redis_id) > 0 }
 }
 
